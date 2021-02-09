9a7fa47f1f8758254cebebfd0b732125c5c11671
harshitgrover-007 <riyagrvr@gmail.com>
2021-02-09 09:32:12 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Axios from 'axios';
+import {v4 as uuidv4} from 'uuid';
+import "./App.css";
+import Recipe from './components/recipe'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [query, setQuery] = useState("");
+    const [recipes, setRecipes] = useState([]);
+
+    const APP_ID = "9ec9abae";
+
+    const APP_KEY = "a02a9895f7299db487061a539e4e48c2";
+
+    const url =`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+    const getData = async () => {
+        const result = await Axios.get(url);
+        setRecipes(result.data.hits)
+        console.log(result);
+        setQuery("");
+    };
+
+    const onChange = (e) => {
+        setQuery(e.target.value);
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        getData();
+    }
+
+    return (
+        <div className="App">
+            <h1>Food Searching App</h1>
+            <form className="search-form" onSubmit={onSubmit}>
+                <input 
+                type="text" 
+                placeholder="Search Food" 
+                autoComplete="off" 
+                onChange={onChange}
+                value={query}
+                 />
+                <input type="submit" value="search" className="btn" />
+            </form>
+            <div className="recipes">
+                {recipes !== [] && recipes.map(recipe => <Recipe key={uuidv4} recipe={recipe} /> )}
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
